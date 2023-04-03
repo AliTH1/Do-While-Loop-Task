aad51120de996caa8ca9a8bb47110496926caeb7
Ali Taghizada <alitaqizade2@gmail.com>
2023-04-03 21:41:54 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿#region Task
+while (true)
+{
+    Console.Write("Reqem daxil edin: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num > 10 && num < 20)
+    {
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Cut ededdir");
+        }
+        else
+        {
+            Console.WriteLine("Tek ededdir");
+        }
+    }
+    else
+    {
+        break;
+    }
+}
+#endregion
