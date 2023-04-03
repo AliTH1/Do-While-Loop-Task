f0df8aedc15b94c7e74a6cdbf1b683097c799e32
Ali Taghizada <alitaqizade2@gmail.com>
2023-04-03 21:46:59 +0400

    added do-while loop

--- Program.cs
+++ Program.cs
@@ -1,23 +1,51 @@
 ﻿#region Task
-while (true)
-{
-    Console.Write("Reqem daxil edin: ");
-    int num = Convert.ToInt32(Console.ReadLine());
 
-    if (num > 10 && num < 20)
-    {
-        if (num % 2 == 0)
-        {
-            Console.WriteLine("Cut ededdir");
-        }
-        else
-        {
-            Console.WriteLine("Tek ededdir");
-        }
-    }
-    else
-    {
-        break;
-    }
-}
+//Do-While
+//do
+//{
+//Console.Write("Reqem daxil edin: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if (num > 10 && num < 20)
+//{
+//    if (num % 2 == 0)
+//    {
+//        Console.WriteLine("Cut ededdir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Tek ededdir");
+//    }
+//}
+//else
+//{
+//    break;
+//}
+//}
+//while (true);
+
+
+
+//While
+//while (true)
+//{
+//    Console.Write("Reqem daxil edin: ");
+//    int num = Convert.ToInt32(Console.ReadLine());
+
+//    if (num > 10 && num < 20)
+//    {
+//        if (num % 2 == 0)
+//        {
+//            Console.WriteLine("Cut ededdir");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Tek ededdir");
+//        }
+//    }
+//    else
+//    {
+//        break;
+//    }
+//}
 #endregion
